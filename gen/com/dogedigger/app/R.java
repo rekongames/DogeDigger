6bad5a3e8fb108f945ebfa3051a51911f5bfd067
Rekon Games <info@rekongames.fi>
2014-02-17 22:23:07 +0200

    tabs working

--- gen/com/dogedigger/app/R.java
+++ gen/com/dogedigger/app/R.java
@@ -27,24 +27,36 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int pager=0x7f080000;
-        public static final int pager_title_strip=0x7f080001;
-        public static final int section_label=0x7f080002;
+        public static final int action_flip=0x7f050001;
+        public static final int action_next=0x7f050000;
+        public static final int action_previous=0x7f050006;
+        public static final int action_settings=0x7f050007;
+        public static final int content=0x7f050005;
+        public static final int pager=0x7f050002;
+        public static final int pager_title_strip=0x7f050003;
+        public static final int section_label=0x7f050004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main_dummy=0x7f030001;
+        public static final int activity_screen_slide=0x7f030001;
+        public static final int fragment_main_dummy=0x7f030002;
+        public static final int fragment_screen_slide_page=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int activity_screen_slide=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int title_section1=0x7f050002;
-        public static final int title_section2=0x7f050003;
-        public static final int title_section3=0x7f050004;
+        public static final int action_finish=0x7f060008;
+        public static final int action_next=0x7f060007;
+        public static final int action_previous=0x7f060006;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int title_screen_slide=0x7f060005;
+        public static final int title_section1=0x7f060002;
+        public static final int title_section2=0x7f060003;
+        public static final int title_section3=0x7f060004;
+        public static final int title_template_step=0x7f060009;
     }
     public static final class style {
         /** 
@@ -68,10 +80,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
